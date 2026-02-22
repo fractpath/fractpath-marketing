20f53f6da5e253207f4aba77655fd4359f6c3f40
Alex <admin@fractpath.com>
2026-02-22 13:47:40 +0000

    sprint10: consume widget v1.0.3 (git-tag install; compute packaged)

--- fractpath-calculator-widget-src/src/widget/FractPathCalculatorWidget.tsx
+++
@@ -1,6 +0,0 @@
-import type { FractPathCalculatorWidgetProps } from "./types.js";
-import { WiredCalculatorWidget } from "./wired.js";
-
-export function FractPathCalculatorWidget(props: FractPathCalculatorWidgetProps) {
-  return <WiredCalculatorWidget {...props} />;
-}
